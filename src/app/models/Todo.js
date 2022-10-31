62fff4eac98f461c0908b296dcd2a740ef4a1fae
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-10-31 21:38:42 +0700

    commit

--- src/app/models/Todo.js
+++ src/app/models/Todo.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
 const Todo = new Schema({
+    _id: Number,
     name: { type: String },
     description: { type: String },
     check: { type: Boolean, default: false },
 }, {
-    timestamps : true,
+    _id: false,
+    timestamps: true,
 })
+Todo.plugin(AutoIncrement)
 
 module.exports = mongoose.model('todo_list', Todo)
